6906371701e45f350908742015a7bf5ac9dfd24a
Bean <bddang@svsu.edu>
2020-10-07 22:28:18 -0400

    Update assignment03.js

--- assignment/assignment03/assignment03.js
+++ assignment/assignment03/assignment03.js
@@ -13,7 +13,7 @@ elem[1].children[4].children[0].onclick
 function add1 (elem) {
   if(elem.children[2].innerHTML == "-") 
     elem.children[2].innerHTML = "1";
-  else if ((parseInt(elem.children[2].innerHTM) / parseInt(elem.children[1].innerHTM)) >= 2){
+  else if ((parseInt(elem.children[2].innerHTM)/parseInt(elem.children[1].innerHTM)) >= 2){
     alert("Score is over spar!");
   }
   else {
