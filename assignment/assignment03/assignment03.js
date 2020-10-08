e459ec4c2f5d02936dba668437147b878ad8f964
Bean <bddang@svsu.edu>
2020-10-07 22:08:51 -0400

    update js

--- assignment/assignment03/assignment03.js
+++ assignment/assignment03/assignment03.js
@@ -8,12 +8,12 @@ for (let i =0;i<rows.length;i++){
 // assign a function to the + button
 elem[1].children[4].children[0].onclick 
   = function(){add1(elem[1]);};
-
+ 
 // create an "add1" function
 function add1 (elem) {
   if(elem.children[2].innerHTML == "-") 
     elem.children[2].innerHTML = "1";
-  else if (parseInt(elem.children[2].innerHTM) / parseInt(elem.children[1].innerHTM) >= 2){
+  else if ((parseInt(elem.children[2].innerHTM) / parseInt(elem.children[1].innerHTM)) >= 2){
     alert("Score is over spar!");
   }
   else {
