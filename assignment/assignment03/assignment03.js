a85d4ca42f340f04792033971f9b844a39cd1cda
Bean <70655926+bddang23@users.noreply.github.com>
2020-10-07 22:13:30 -0400

    Update assignment03.js

--- assignment/assignment03/assignment03.js
+++ assignment/assignment03/assignment03.js
@@ -11,8 +11,9 @@ elem[1].children[4].children[0].onclick
  
 // create an "add1" function
 function add1 (elem) {
-  if(elem.children[2].innerHTML == "-") 
+  if(elem.children[2].innerHTML == "-") {
     elem.children[2].innerHTML = "1";
+  {
   else if ((parseInt(elem.children[2].innerHTM) / parseInt(elem.children[1].innerHTM)) >= 2){
     alert("Score is over spar!");
   }
